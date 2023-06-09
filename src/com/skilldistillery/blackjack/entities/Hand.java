a78a0d0f687e4a2bd85fedf9a176ba3413e38bd4
Dean Coffman <deansdetail@gmail.com>
2023-06-09 16:42:24 -0400

    send addition

---
+++ src/com/skilldistillery/blackjack/entities/Hand.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.blackjack.entities;
+
+public abstract class Hand {
+
+	public abstract int getHandValue();
+
+}
--- src/com/skilldistillery/blackjack/testing/TestTheDeck.java
+++ src/com/skilldistillery/blackjack/testing/TestTheDeck.java
@@ -1,15 +1,22 @@
 package com.skilldistillery.blackjack.testing;
 
+import com.skilldistillery.blackjack.entities.Card;
 import com.skilldistillery.blackjack.entities.Deck;
 
 public class TestTheDeck {
 
+//	deck.shuffle();
 	public static void main(String[] args) {
-		// For each of the 52 cards in your deck, display each card. 
-		Deck deckOfCards = new Deck();
-		System.out.println(deckOfCards.checkDeckSize());
+	Deck deckOfCards = new Deck();
 		
+		final int initialDeckSize = deckOfCards.checkDeckSize();
 		
+		for (int cardNumber = 0; cardNumber < initialDeckSize; cardNumber++) {
+			Card card = deckOfCards.dealCard();
+			
+			System.out.println("cardNumber: " + cardNumber + " " + card + " " + deckOfCards.checkDeckSize());
+		}
+	
 		
 
 	}
